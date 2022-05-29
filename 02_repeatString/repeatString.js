acdc936ddc20ab0fcfbf2691a5d61f7686062ddf
acewvrs <song.junyoung64@gmail.com>
2022-05-28 20:56:49 -0500

    Solve exercise 1 and 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(string_input, n) {
+  if (n < 0) {
+      return 'ERROR';
+  }
+  else if (n === 0) {
+      return '';
+  }
 
+  concatenated_str = string_input;
+  for (let i = 0; i < n - 1; i++) {
+    concatenated_str += string_input;
+  }
+  return concatenated_str;
 };
 
 // Do not edit below this line
